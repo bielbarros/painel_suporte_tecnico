a15ca6f017ecbfa016a57302c805e6eee603b0e7
bielbarros <gabrielbarrosg11@gmail.com>
2025-06-23 15:11:01 -0300

    feat: Define Get-InfoSistema function to retrieve system information

--- funcoes/info_sistema.ps1
+++ funcoes/info_sistema.ps1
@@ -1,7 +1,10 @@
-$info = @{
-    "Computador" = $env:COMPUTERNAME
-    "Usuário" = $env:USERNAME
-    "Versão do Windows" = (Get-CimInstance Win32_OperatingSystem).Caption
-    "Modelo do PC" = (Get-CimInstance Win32_ComputerSystem).Model
+function Get-InfoSistema {
+    $info = @{
+        "Computador" = $env:COMPUTERNAME
+        "Usuário" = $env:USERNAME
+        "Versão do Windows" = (Get-CimInstance Win32_OperatingSystem).Caption
+        "Modelo do PC" = (Get-CimInstance Win32_ComputerSystem).Model
+    }
+    return $info
 }
-return $info
+Export-ModuleMember -Function Get-InfoSistema
