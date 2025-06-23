05c1c023d5dd3d4c535a887f524856a8c6bf0abf
bielbarros <gabrielbarrosg11@gmail.com>
2025-06-23 15:11:31 -0300

    feat: Implement Get-Diagnostico function to retrieve disk, internet, and antivirus information

--- funcoes/diagnostico.ps1
+++ funcoes/diagnostico.ps1
@@ -1,2 +1,13 @@
-$drives = Get-PSDrive -PSProvider 'FileSystem' | Select-Object Name, Used, Free
-return $drives
+function Get-Diagnostico {
+    $discos = Get-PSDrive -PSProvider 'FileSystem' | Select-Object Name, Used, Free
+    $ping = Test-Connection -ComputerName google.com -Count 2 -Quiet
+    $antivirus = Get-CimInstance -Namespace root\\SecurityCenter2 -ClassName AntivirusProduct | Select-Object displayName, productState
+
+    $resultado = @{
+        "Discos" = $discos
+        "Internet Ativa" = $ping
+        "Antivírus" = $antivirus
+    }
+    return $resultado
+}
+Export-ModuleMember -Function Get-Diagnostico
